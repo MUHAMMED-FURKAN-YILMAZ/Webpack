bd46474c175d781dc6b1685598df652d89a1b2fc
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2023-01-03 00:08:08 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { setItem } from "./utils/storage";
 import "./index.scss";
-import "bootstrap";
 
 document.getElementById("btnClick").addEventListener("click", () => {
   alert("Hello Webpack");
+  alert("v1");
 
   setItem(
     "token",
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,18 @@
-const path = require("path");
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    watchFiles: ["src/**/*.html", "src/**/*.scss"],
+  },
+  output: {
+    filename: "./js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: false,
+  },
   module: {
     rules: [
       {
@@ -19,4 +25,4 @@ module.exports = {
       },
     ],
   },
-};
+});
