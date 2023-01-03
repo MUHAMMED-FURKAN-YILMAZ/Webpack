18e3a2b568e81125335f75b49ea1378622cce7e1
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2023-01-03 21:32:42 +0300

    v3

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ document.getElementById("btnClick").addEventListener("click", () => {
   alert("Hello Webpack");
   alert("v1");
 
+  const arr = [123, 56, 3, 67, 2, 67];
+  console.log(getArray(arr));
   setItem(
     "token",
     "klsahjdkhaskhdjkahskdjhaskjdhjkashkdasdasedwqfrdsfgsdfdjhasjd"
   );
 });
+
+const getArray = (arr) => {
+  return arr.filter((x) => x > 10).reduce((t, x) => x + t, 0);
+};
